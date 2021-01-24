3a608a97bce4e510ee7ac1ee6dcb8832ac2f3a67
Patryk Staniewski <exihuatl@gmail.com>
2021-01-24 15:28:14 +0100

    add category view

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 10.0)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                            CategoryItem(landmark: landmark)
+                        }
+                    }
+                }
+            }
+            .frame(height: 185)
+            .padding(.bottom, 10)
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarks[0].category.rawValue,
+            items: Array(landmarks.prefix(4))
+        )
+    }
+}
